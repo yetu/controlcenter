4cfd6098d95995848c249c36a14bd001b14efb76
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-03-01 02:38:14 +0100

    Experiment with build process

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,4 +1,5 @@
 'use strict';
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	context: __dirname,
@@ -18,16 +19,17 @@ module.exports = {
 			'actions': __dirname + '/app/actions/actions',
 			'screens': __dirname + '/app/screens',
 			'stores': __dirname + '/app/stores',
-			'common': __dirname + '/app/common'
+			'common': __dirname + '/app/common',
+			'mixins': __dirname + '/app/common/mixins'
 		},
     extensions: ['', '.js', '.jsx']
 	},
 	module: {
 		loaders: [
 			{test: /\.jsx$/, loader: 'jsx-loader?insertPragma=React.DOM&harmony'},
-			{test: /\.scss$/, loader: 'css!autoprefixer!sass?' +
+			{test: /\.scss$/, loader: ExtractTextPlugin.extract('style','css!autoprefixer!sass?' +
 			'includePaths[]=' + __dirname + '/app/project-setup' +
-			'&includePaths[]=' + __dirname + '/bower_components/foundation/scss'},
+			'&includePaths[]=' + __dirname + '/bower_components/foundation/scss')},
 			{test: /\.(png|jpg)$/, loader: 'url?limit=32768'},
 			{test: /\.jade$/, loader: 'jade'},
 			{test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&minetype=application/font-woff'},
@@ -38,5 +40,10 @@ module.exports = {
 	},
 	externals: {
 	},
+	plugins: [
+		new ExtractTextPlugin("style.css", {
+			allChunks: true
+		})
+	],
 	devtool: 'eval'
 };

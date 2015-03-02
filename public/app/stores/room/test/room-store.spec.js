d45b4b79e89dd5cda802788de51d799942b96550
Lars Blumberg <lars.blumberg@yetu.com>
2015-03-02 18:26:42 +0100

    Added a test for onCreateRoom

---
+++ public/app/stores/room/test/room-store.spec.js
@@ -0,0 +1,20 @@
+var React = require('react/addons'),
+    TestUtils = React.addons.TestUtils;
+
+var roomStore = require('../room-store.js');
+
+describe('RoomStore', function () {
+    describe('onCreateRoom', function () {
+        it('adds a new room to the store', function () {
+            var roomsBefore = roomStore.getRooms();
+            roomStore.onCreateRoom();
+            var roomsAfter = roomStore.getRooms();
+            
+            expect(roomsAfter.length).toEqual(roomsBefore.length + 1);
+            
+            var createdRoom = roomsAfter[roomsAfter.length - 1];
+            expect(createdRoom.title).toBeDefined();
+            expect(createdRoom.devices.length).toEqual(0);
+        });
+    });
+});

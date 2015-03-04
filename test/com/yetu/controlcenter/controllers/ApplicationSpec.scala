27f27e9396b48985ab39fb1cb6a974a5128ea602
Engin Yoeyen <engin.yoyen@gmail.com>
2015-03-04 22:33:54 +0100

    Add household entry point for proxying all request to the household api

--- test/com/yetu/controlcenter/controllers/ApplicationSpec.scala
+++ test/com/yetu/controlcenter/controllers/ApplicationSpec.scala
@@ -4,23 +4,41 @@ import com.yetu.controlcenter.base.BaseRoutesSpec
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
-class ApplicationSpec extends BaseRoutesSpec{
-  val indexUrl = "/"
 
-  s"non-authenticated GET request on $indexUrl" must {
-    "return a 303 response" in {
-      val Some(response) = route(FakeRequest(GET, indexUrl))
-      status(response) mustEqual (SEE_OTHER)
+class ApplicationSpec extends BaseRoutesSpec {
+    val indexUrl = "/"
+    val profile = "/profile"
+    val household = "/household"
+
+
+    s"non-authenticated GET request on $indexUrl" must {
+        "return a 303 response" in {
+            val Some(response) = route(FakeRequest(GET, indexUrl))
+            status(response) mustEqual (SEE_OTHER)
+        }
+
     }
 
-  }
-
-  s"authenticated GET request on $indexUrl" must {
-    "return a valid 200 response" in {
-      val response = getRequestAuthenticated(indexUrl)
-      status(response) mustEqual (OK)
-      contentAsString(response) must include("Control Center")
+    s"authenticated GET request on $indexUrl" must {
+        "return a valid 200 response" in {
+            val response = getRequestAuthenticated(indexUrl)
+            status(response) mustEqual (OK)
+            contentAsString(response) must include("Control Center")
+        }
     }
-  }
+
+    s"GET request on $profile" must {
+        "return a 500 as there are no access tokens" in {
+            val response = getRequestAuthenticated(profile)
+            status(response) mustEqual (INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    s"GET request on $household" must {
+        "return a 500 as there are no access tokens" in {
+            val response = getRequestAuthenticated(household)
+            status(response) mustEqual (INTERNAL_SERVER_ERROR)
+        }
+    }
 
 }

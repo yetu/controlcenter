42e550d8711c4a22db85bc6938a1cce2d7169fd5
Lars Blumberg <lars.blumberg@yetu.com>
2015-03-04 16:59:20 +0100

    Using grid layout for device rows; display (dis)connected image

--- public/app/screens/main/sections/devices/device/device.jsx
+++ public/app/screens/main/sections/devices/device/device.jsx
@@ -3,15 +3,34 @@ var styleMixin = require('mixins/style-mixin');
 
 var Device = React.createClass({
   mixins: [ styleMixin(require('./style.scss')) ],
+
   render: function render () {
     return (
-      <div className='cc-devices__device row'>
-        <div className='cc-devices__device--title small-6 column'>{this.props.device.title}</div>
-        <div className='cc-devices__device--type small-6 column'>{this.props.device.type}</div>
-        <div className='cc-devices__device--control small-4 column'>Control</div>
-        <div className='cc-devices__device--state small-4 column'>{this.props.device.state}</div>
+      <div className="cc-device__row">
+        <div className="cc-device__title-column">{this.props.device.title}</div>
+        <div className="cc-device__type-column">{this.props.device.type}</div>
+        <div className="cc-device__control-column">Control</div>
+        <div className={'cc-device__state-image-column ' + this.stateImageClass()}>&nbsp;</div>
+        <div className="cc-device__state-text-column">{this.connectedText()}</div>
       </div>
     );
+  },
+
+  stateImageClass: function setImageClass () {
+    var name = 'cc-device__state-img-';
+    if (this.props.device.state === 'connected') {
+      return name + 'connected';
+    } else {
+      return name + 'disconnected';
+    }
+  },
+
+  connectedText: function connectedText () {
+    if (this.props.device.state === 'connected') {
+      return 'Connected';
+    } else {
+      return 'Disconnected';
+    }
   }
 });
 

e94ebeb686ee66487dce2ed15a686b8c88b28f48
Elisa Hilprecht <elisa.hilprecht@yetu.com>
2015-03-10 14:20:24 +0100

    mark link as active, if subpath of link is opened (to see marked link when device detail page opened)

--- public/app/screens/main/navigation/navigation.jsx
+++ public/app/screens/main/navigation/navigation.jsx
@@ -1,21 +1,30 @@
 var React = require('react'),
-  Link = require('react-router').Link,
+  cx = require('react/lib/cx'),
+  Router = require('react-router'),
+  State = Router.State,
+  Link = Router.Link,
   styleMixin = require('mixins/style-mixin');
 
 var Navigation = React.createClass({
-  mixins: [ styleMixin(require('./style.scss')) ],
+  mixins: [ styleMixin(require('./style.scss')), State],
   render: function render () {
     // TODO: Use mixins instead of CSS classes
     // TODO: Pass number of elements to mixin instead of specifying class name
     var classes = 'cc_navigation icon-bar two-up ' + this.props.orientation;
 
     var items = this.props.items.map(function mapper (item, i) {
+      var classNames = cx({
+        'cc_navigation__link': true,
+        'item': true,
+        //also mark link as active, if subpath of link is opened
+        'active': this.getPath().indexOf('/'+item.linkTo)===0
+      });
       return (
-        <Link to={item.linkTo} key={i} className='cc_navigation__link item'>
+        <Link to={item.linkTo} key={i} className={classNames}>
           <label>{item.title}</label>
         </Link>
       );
-    });
+    }.bind(this));
 
     return (
       <div className={classes}>

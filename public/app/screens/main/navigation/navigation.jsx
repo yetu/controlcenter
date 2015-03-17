b8ff2e89a187067780ff68b28da5637652655eda
Mr_Mig <alexey.migutsky@gmail.com>
2015-03-17 14:06:51 +0100

    Merged master

--- public/app/screens/main/navigation/navigation.jsx
+++ public/app/screens/main/navigation/navigation.jsx
@@ -9,21 +9,23 @@ var Navigation = React.createClass({
   mixins: [styleMixin(require('./style.scss')), State],
   render: function render () {
     var items = this.props.items.map(function mapper (item, i) {
+
       var classNames = cx({
         'cc_navigation__link': true,
         'item': true,
         // also mark link as active, if subpath of link is opened
         'active': this.getPath().indexOf('/' + item.linkTo) === 0
       });
+
       return (
         <Link to={item.linkTo} key={i} className={classNames}>
           <div className={'cc_navigation__link-image ' + item.image} />
-          <span className="cc_navigation__link-label">{item.title}</span>
+          <label className="cc_navigation__link-label">{item.title}</label>
         </Link>
       );
+
     }.bind(this));
 
-    // TODO: Use "icon-bar" mixin instead of CSS classes
     // TODO: Pass number of elements to mixin instead of specifying class name
     return (
       <div className={'cc_navigation icon-bar two-up ' + this.props.orientation}>

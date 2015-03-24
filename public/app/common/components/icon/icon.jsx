79e260cd6d72bbe8c066b249edd766217fcf3737
Alexander Gerasimov <leo.daquirm@gmail.com>
2015-03-24 18:37:12 +0100

    Add Icon component

---
+++ public/app/common/components/icon/icon.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var Pure = require('react/addons').addons.PureRenderMixin;
+var cx = require('react/lib/cx');
+var styleMixin = require('mixins/style-mixin');
+
+module.exports = React.createClass({
+  mixins: [styleMixin(require('./style.scss')), Pure],
+
+  getIconTypeClass: function getIconTypeClass () {
+    return this.props.type
+      ? 'cc-icon-' + this.props.type
+      : '';
+  },
+
+  render: function renderButton () {
+
+    var className = cx({
+      'cc-icon': true,
+      [this.getIconTypeClass()]: true
+    });
+
+    return (
+      <span className={ className }></span>
+    );
+  }
+
+});

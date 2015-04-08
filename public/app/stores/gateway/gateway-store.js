53dfe456ed132580bb59f7e0bc534396289312aa
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-04-08 15:03:42 +0200

    Added gateway store

---
+++ public/app/stores/gateway/gateway-store.js
@@ -0,0 +1,36 @@
+var Reflux = require('reflux');
+
+var deviceActions = require('actions/device');
+var gatewayService = require('services/devices/gateway-service');
+
+module.exports = Reflux.createStore({
+  init: function init () {
+    this.listenTo(deviceActions.fetchGateway, this.onFetchGateway);
+    this.onFetchGateway();
+  },
+
+  getInitialState: function getInitialState () {
+    return {
+      model: {}
+    };
+  },
+
+  onFetchGateway: function onFetchGateway () {
+    var self = this;
+    gatewayService.fetchGatewayInfo()
+      .subscribe(self.updateModel.bind(self),
+      self.updateError.bind(self));
+  },
+
+  updateModel: function updateModel (gateway) {
+    this.trigger({
+      model: gateway
+    });
+  },
+
+  updateError: function updateError (error) {
+    this.trigger({
+      error: error
+    });
+  }
+});

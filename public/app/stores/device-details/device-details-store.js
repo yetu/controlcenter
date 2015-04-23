4e50550131b63da5d8d5476a4dafbdbb4a6eabfb
Alexander Gerasimov <leo.daquirm@gmail.com>
2015-04-23 17:04:06 +0200

    Reuse data from the device list store if it has already been fetched

--- public/app/stores/device-details/device-details-store.js
+++ public/app/stores/device-details/device-details-store.js
@@ -6,11 +6,13 @@ var DevicesService = require('services/devices/devices-service');
 
 var DeviceHelpers = require('helpers/device');
 
+var deviceListStore = require('stores/device-list');
 var deviceDetailsStore = Reflux.createStore({
 
   init: function init () {
     this.listenTo(DeviceActions.fetchDeviceById, this.onFetchDeviceById);
     this.listenTo(DeviceActions.invokeAction, this.onInvokeAction);
+    this.listenTo(deviceListStore, this.onDeviceListStoreUpdate);
 
     this.device = this.createModel();
   },
@@ -33,13 +35,25 @@ var deviceDetailsStore = Reflux.createStore({
 
   createModel: function createModel (device) {
     return {
-      model: device ? this.augmentDevice(device) : null,
+      model: device || null,
       error: null
     };
   },
 
+  onDeviceListStoreUpdate: function onDeviceListStoreUpdate (data) {
+    this.deviceById = data.deviceById;
+  },
+
   onFetchDeviceById: function onFetchDeviceById (deviceId) {
-    DevicesService.fetchDeviceById(deviceId).subscribe(this.updateModel, this.updateError);
+    if (this.deviceById && this.deviceById[deviceId]) {
+      this.updateModel(this.deviceById[deviceId]);
+    } else {
+      DevicesService.fetchDeviceById(deviceId).subscribe(this.augmentAndUpdateDevice, this.updateError);
+    }
+  },
+
+  augmentAndUpdateDevice: function augmentAndUpdateDevice (device) {
+    this.updateModel(this.augmentDevice(device));
   },
 
   onInvokeAction: function doAction (action, data) {

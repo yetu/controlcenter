b20830f327efff7657c01bad0dd88cb544e665ca
Joe Schaul <joe.schaul@gmail.com>
2015-04-28 11:43:06 +0200

    cleanup configuration values

--- app/com/yetu/controlcenter/Global.scala
+++ app/com/yetu/controlcenter/Global.scala
@@ -2,10 +2,12 @@ package com.yetu.controlcenter
 
 import play.api.GlobalSettings
 
+import com.yetu.play.authenticator.AuthenticatorGlobal
+
 // Global object as referenced in reference.conf
 object Global extends Global
 
 // settings to use in the application. Can be overridden for tests in test/FakeGlobal
-trait Global extends GlobalSettings with com.yetu.play.authenticator.Global {
+trait Global extends GlobalSettings with AuthenticatorGlobal {
 
 }
--- clean_dev.sh
+++ clean_dev.sh
@@ -5,32 +5,26 @@ echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	read -p "Have you added '127.0.0.1 local.yetudev.com' to your /etc/hosts file?" -n 1 -r
-	echo
 
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		echo "Clean up stale class-files..."
-		sbt clean compile
+  echo "Clean up stale class-files..."
+  sbt clean compile
 
-		echo "Installing global npm modules..."
-		npm install -g bower webpack
+  echo "Installing global npm modules..."
+  npm install -g bower webpack
 
-		echo "Installing local npm modules..."
-		npm install
+  echo "Installing local npm modules..."
+  npm install
 
-		echo "Installing bower modules..."
-		echo
-		cd ./public
-		bower install
-		cd ..
+  echo "Installing bower modules..."
+  echo
+  cd ./public
+  bower install
+  cd ..
 
-		echo "Done!"
-		echo
-		echo "Now you can run ./dev.sh to run the project in dev mode!"
-	else
-	echo "Please, update your /etc/hosts before running homescreen."
-	fi
+  echo "Done!"
+  echo
+  echo "Now you can run ./dev.sh to run the project in dev mode!"
+
 else
 
 echo "You definitely should read README.md!"

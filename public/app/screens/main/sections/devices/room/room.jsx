9a1a22a9dbcf4344a31e3b668605149355462712
Lars Blumberg <lars.blumberg@yetu.com>
2015-05-26 17:41:12 +0200

    Vertically center room’s title

--- public/app/screens/main/sections/devices/room/room.jsx
+++ public/app/screens/main/sections/devices/room/room.jsx
@@ -24,8 +24,8 @@ var Room = React.createClass({
     return (
       <div className='cc-room'>
         <div className='row fixed-height-1 group-header'>
-          <div className='columns small-14 quarter-padded-left'>
-            <h3 className='cc-room__header'>{this.props.title}</h3>
+          <div className='column small-14 quarter-padded-left'>
+            <h3 className='align-text-top'>{this.props.title}</h3>
           </div>
         </div>
         <Gateway/>

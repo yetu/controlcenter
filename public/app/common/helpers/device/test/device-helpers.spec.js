8e9eb58e2050845f13425209b566cb33e94dac5f
Lars Blumberg <lars.blumberg@yetu.com>
2015-06-03 09:42:37 +0200

    Updated test to reflect changed nest webservice name

--- public/app/common/helpers/device/test/device-helpers.spec.js
+++ public/app/common/helpers/device/test/device-helpers.spec.js
@@ -5,7 +5,7 @@ describe('DeviceHelpers', function () {
   describe('.augmentDevice()', function () {
     it('flags nest webservice as hidden', function () {
       var nestWebservice = {
-        properties: { name: 'WEBSERVICE oauth', displayType: 'nest' },
+        properties: { name: 'nest account', displayType: 'nest' },
         actions: [],
         components: [ { properties: { type: 'nest', capabilities: [ { } ] } } ],
         links: [{ rel: ['self'] }]

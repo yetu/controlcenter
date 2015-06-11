d874783c21033fc7be6e486baed3db5c82a63501
Alexander Gerasimov <leo.daquirm@gmail.com>
2015-06-11 10:40:29 +0200

    Fix Setup App link in the gateway details

--- public/app/stores/gateway/gateway-store.js
+++ public/app/stores/gateway/gateway-store.js
@@ -1,4 +1,6 @@
 var Reflux = require('reflux');
+var _ = require('lodash');
+var platform = require('platform');
 
 var deviceActions = require('actions/device');
 var gatewayService = require('services/devices/gateway-service');
@@ -9,24 +11,45 @@ module.exports = Reflux.createStore({
     this.onFetchGateway();
   },
 
+  getSetupAppUrl: function getSetupAppUrl () {
+
+    function isOSX () {
+      var userAgentString = navigator.userAgent;
+      var environment = platform.parse(userAgentString);
+      return /OS X/.test(environment.os.family);
+    }
+
+    if (isOSX(platform)) {
+      return window.yetu.config.setupDownloadUrlMac;
+    } else {
+      return window.yetu.config.setupDownloadUrlWin;
+    }
+  },
+
+  createModel: function createModel (data) {
+    return _.assign(data, {
+      setupAppUrl: this.getSetupAppUrl()
+    });
+  },
+
   getInitialState: function getInitialState () {
     // fetch gateway status on each page view
     // as we don't have polling
     this.onFetchGateway();
     return {
-      model: {}
+      model: this.createModel({})
     };
   },
 
   onFetchGateway: function onFetchGateway () {
-    var self = this;
     gatewayService.fetchGatewayInfo()
-      .subscribe(self.updateModel.bind(self), self.updateError.bind(self));
+      .subscribe(this.updateModel, this.updateError);
   },
 
-  updateModel: function updateModel (gateway) {
+  updateModel: function updateModel (gatewayData) {
+    var model = this.createModel(gatewayData);
     this.trigger({
-      model: gateway
+      model: model
     });
   },
 
